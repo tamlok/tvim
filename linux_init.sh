41c16a847d5366576cb01713a454d01c125c3c3c
Le Tan <tamlokveer@gmail.com>
2018-05-07 09:38:52 +0800

    refine linux_init.sh

--- linux_init.sh
+++ linux_init.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 echo 'Coping .vimrc'
-cp .vimrc ~/.vimrc
+cp -f .vimrc ~/.vimrc
 
 echo 'Coping detorte colorscheme'
-mkdir ~/.vim/colors
-cp -r detorte/colors ~/.vim/colors
+mkdir -p ~/.vim/colors
+cp -fr detorte/colors ~/.vim/colors
 
 echo 'Coping plug.vim'
-mkdir ~/.vim/autoload
+mkdir -p ~/.vim/autoload
 cp -f plug.vim ~/.vim/autoload/plug.vim
 
 # Add support for markdown in Tagbar plugin
 echo 'Coping markdown2ctags.py'
-cp markdown2ctags.py ~/.vim/
+cp -f markdown2ctags.py ~/.vim/
