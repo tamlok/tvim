d954138f44e05fce181a96b4886cb6a871e656da
Le Tan <tamlokveer@gmail.com>
2019-07-26 11:57:42 +0000

    refine install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,91 @@
 #!/bin/sh
-echo 'Coping .vimrc'
-cp -f .vimrc ~/.vimrc
+# Vim's Setup Utils on Linux
+# Le Tan (tamlokveer at gmail.com)
+# https://github.com/tamlok/tvim
 
-echo 'Coping detorte colorscheme'
-mkdir -p ~/.vim/colors
-cp -fr detorte/colors/* ~/.vim/colors/
+list_action() {
+    echo 'Actions:'
+    echo '1. Setup Vim'
+    echo '2. Setup Neovim'
+    echo '3. Exit'
+}
 
-echo 'Coping plug.vim'
-mkdir -p ~/.vim/autoload
-cp -f plug.vim ~/.vim/autoload/plug.vim
+check_detorte() {
+    local vim_files_folder=$1
+    echo "Check detorte in $vim_files_folder"
+    cp -r -f ./detorte/colors "$vim_files_folder/"
+}
 
-# Add support for markdown in Tagbar plugin
-echo 'Coping markdown2ctags.py'
-cp -f markdown2ctags.py ~/.vim/
+check_plug() {
+    local plug_folder="$1/autoload"
+    mkdir -p "$plug_folder" 2> /dev/null
+    echo "Check Plug in $plug_folder"
+    cp -f ./utils/plug.vim "$plug_folder/"
+}
+
+check_markdowntoctags() {
+    local vim_files_folder=$1
+    echo "Check markdown2ctags.py in $vim_files_folder"
+    cp -f ./utils/markdown2ctags.py "$vim_files_folder/"
+}
+
+check_coc_settings() {
+    local vim_files_folder=$1
+    echo "Check coc-settings.json in $vim_files_folder"
+    cp -f ./utils/coc-settings.json "$vim_files_folder/"
+}
+
+check_vimrc() {
+    local home_folder=$HOME
+    echo "Check .vimrc in $home_folder"
+    cp -f ./vimrc "$home_folder/.vimrc"
+}
+
+check_scripts() {
+    local vim_files_folder=$1
+    echo "Check scripts in $vim_files_folder"
+    rm -rf "$vim_files_folder/scripts" 2> /dev/null
+    cp -r -f ./scripts "$vim_files_folder/"
+}
+
+setup_vim() {
+    echo '==Setup Vim=='
+    local vim_files_folder="$HOME/.vim"
+
+    mkdir "$vim_files_folder" 2> /dev/null
+
+    check_detorte $vim_files_folder
+    check_plug $vim_files_folder
+    check_markdowntoctags $vim_files_folder
+    check_coc_settings $vim_files_folder
+    check_vimrc
+    check_scripts $vim_files_folder
+}
+
+setup_neovim() {
+    echo '==Setup Neovim=='
+    local vim_files_folder="$HOME/.config/nvim"
+    echo 'Not implemented yet!'
+}
+
+echo 'Welcome! Let me help you setup Vim on Linux'
+
+list_action
+
+read -p 'Action: ' action
+
+case $action in
+    '1')
+        setup_vim
+        ;;
+
+    '2')
+        setup_neovim
+        ;;
+
+    *)
+        echo 'Exit'
+        ;;
+esac
+
+exit $?
--- scripts/coc.nvim/package.vim
+++ scripts/coc.nvim/package.vim
@@ -1,8 +1,13 @@
-Plug 'yatli/coc-powershell', {'do': { -> coc#powershell#install()} }
+if g:tvim_os == 'win'
+    Plug 'yatli/coc-powershell', {'do': { -> coc#powershell#install()} }
+else
+    Plug 'yatli/coc-powershell'
+endif
+
 Plug 'neoclide/coc-json'
 Plug 'neoclide/coc-tsserver'
 if g:tvim_os == 'win'
-    Plug 'neoclide/coc.nvim', {'tag': '*', 'do': 'install.cmd'}
+    Plug 'neoclide/coc.nvim', {'do': 'install.cmd'}
 else
-    Plug 'neoclide/coc.nvim', {'tag': '*', 'do': './install.sh'}
+    Plug 'neoclide/coc.nvim', {'do': './install.sh'}
 endif
